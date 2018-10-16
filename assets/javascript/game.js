2987492e3067e55e24c8fd22a16c085224135349
Yaye Fatou Thiam <yayefatouthiam@Yayes-MacBook-Air.local>
2018-10-15 21:16:44 -0500

    initial version

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//Javascript file
+
+//Variables creation
+var wins = 0;
+var currentWord;
+var numberOfGuesses = 0;
+var letterAlreadyGuessed = [];
+
+//the array of words
+var wordsToGuess = {
+
+}
+
